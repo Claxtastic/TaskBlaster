2849d3b90d8c1808271065afacac5188836a0b02
claxtont <claxtons4@gmail.com>
2020-04-16 16:34:02 -0500

    Added list and checkboxes to subtasks

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -7,9 +7,9 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import { withStyles } from '@material-ui/core/styles';
+import { Checkbox } from '@material-ui/core';
 
 const styles = theme => ({
     card: {
@@ -22,28 +22,67 @@ const styles = theme => ({
 });
 
 class Task extends React.Component {
-    render () {
-      const { classes } = this.props;
-      console.log(this.props.subTasks);
-  
-      return (
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.cardTypography}>
-              {this.props.title}
-            </Typography>
-            {/* TODO: make this a list */}
-            <List className="subTasks">
-              {this.props.subTasks.map(subTask => subTask)}
-            </List>
-          </CardContent>
-          <CardActions>
-            {/* Arrow function because we don't want the function to run when the Task component is created */}
-            <Button size="small" onClick={() => this.props.removeTask(this.props.number)}>REMOVE</Button>
-            <Button size="small" onClick={() => null}>DONE</Button>
-          </CardActions>
-        </Card>
-      )}
+  constructor() {
+    super();
+    this.state = {
+      checked: [],
+    }
+  }
+
+  handleCheck = (index) => () => {
+    const currentIndex = this.state.checked.indexOf(index);
+    const newChecked = [...this.state.checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(index);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked
+    });
+  }
+
+  render () {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.cardTypography}>
+            {this.props.title}
+          </Typography>
+          {/* TODO: make this a list */}
+          <List>
+            {this.props.subTasks.map((subTask, index) => { 
+              return (
+                <ListItem 
+                  key={index}
+                  dense
+                  button
+                  onClick={this.handleCheck(index)}>
+                    <ListItemIcon>
+                      <Checkbox
+                        edge="start"
+                        checked={this.state.checked.indexOf(index) !== -1}
+                        tabIndex={-1}
+                        disableRipple
+                      />
+                    </ListItemIcon>
+                    <ListItemText primary={subTask} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </CardContent>
+        <CardActions>
+          {/* Arrow function because we don't want the function to run when the Task component is created */}
+          <Button size="small" onClick={() => this.props.removeTask(this.props.number)}>REMOVE</Button>
+          <Button size="small" onClick={() => null}>DONE</Button>
+        </CardActions>
+      </Card>
+    )}
 }
 
 export default withStyles(styles)(Task);
