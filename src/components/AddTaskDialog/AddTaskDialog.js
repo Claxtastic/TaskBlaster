03504742f85aa93ef60c84b0dd7e78f70fb7c3b3
claxtont <claxtons4@gmail.com>
2020-04-16 02:22:43 -0500

    Implemented a field for the tasks and a message for no zero tasks

--- src/components/AddTaskDialog/AddTaskDialog.js
+++ src/components/AddTaskDialog/AddTaskDialog.js
@@ -5,36 +5,57 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
 
 class AddTaskDialog extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { open: true };
+        this.state = { 
+            title: "",
+        };
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.addTask(this.state.title);
+    }
+
+    changeHandler = (event) => {
+        this.setState({ title: event.target.value });
     }
 
     render() {
         return (
             <div>
                 <Dialog
-                    open={this.state.open}
+                    open={true}
                     aria-labelledby="add-new-dialog"
                 >
-                    <DialogTitle>
-                        Add New Task
-                    </DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Click save to add a new task
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={() => this.props.addNewTask()} color="primary">
-                            Save
-                        </Button>
-                        <Button onClick={() => this.props.hideDialog()} color="primary">
-                            Close
-                        </Button>
-                    </DialogActions>
+                    <form onSubmit={this.submitHandler}>
+                        <DialogTitle>
+                            Add New Task
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Task Title"
+                                    onChange={this.changeHandler}
+                                    fullWidth
+                                />
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button type="submit" color="primary">
+                                Save
+                            </Button>
+                            <Button onClick={() => this.props.hideDialog()} color="primary">
+                                Close
+                            </Button>
+                        </DialogActions>
+                    </form>
                 </Dialog>
             </div>
         )
