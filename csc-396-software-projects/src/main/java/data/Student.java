27a0ed7b851f6dfd6c4457c13c635bf0af857b30
Stephen Kim <stephen.kim04@gmail.com>
2020-05-19 19:35:36 -0500

    Committed relationships

--- csc-396-software-projects/src/main/java/data/Student.java
+++ csc-396-software-projects/src/main/java/data/Student.java
@@ -10,10 +10,23 @@ package data;
  *
  */
 public class Student implements User {
-	
-	//Student constructor
-	private Student() {
-		
+
+	//Create variables
+	private final String _name;
+	private final String _password;
+
+	//Student constructor with name and password
+	Student(String name, String password) {
+		//If name is null, throw NullPointerException
+		if (name == null ) {
+			throw new NullPointerException(("Name cannot be null"));
+		}
+		//If name is empty string, throw IllegalArgumentException
+		if (name == "" || name == " ") {
+			throw new IllegalArgumentException("Name cannot be empty string");
+		}
+		this._name = name;
+		this._password = password;
 	}
 
 }
